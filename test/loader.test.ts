87b280fbc2dd8e707882407573ad7454d73b3dc5
Francisco Madeira <xico2k@gmail.com>
2022-03-24 14:23:57 +0000

    test: fix failling test

--- test/loader.test.ts
+++ test/loader.test.ts
@@ -8,13 +8,14 @@ it('creates a file for each lang', () => {
 
     expect(files.length).toBe(3);
     expect(files[0].name).toBe('php_en.json');
-    expect(files[1].name).toBe('php_pt.json');
+    expect(files[1].name).toBe('php_fr.json');
+    expect(files[2].name).toBe('php_pt.json');
 
     const langEn = JSON.parse(fs.readFileSync(files[0].path).toString());
     expect(langEn['auth.failed']).toBe('These credentials do not match our records.');
     expect(langEn['auth.foo.level1.level2']).toBe('baren');
 
-    const langPt = JSON.parse(fs.readFileSync(files[1].path).toString());
+    const langPt = JSON.parse(fs.readFileSync(files[2].path).toString());
     expect(langPt['auth.failed']).toBe('As credenciais indicadas não coincidem com as registadas no sistema.');
     expect(langPt['auth.foo.level1.level2']).toBe('barpt');
 });

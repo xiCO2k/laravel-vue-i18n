64be72e7924a0b91edd3fb6d83b54a8cfd52f54b
Andrei Ioniță <hi@andrei.io>
2022-07-27 10:07:27 +0100

    fix typeof check

--- src/utils/has-php-translations.ts
+++ src/utils/has-php-translations.ts
@@ -8,7 +8,7 @@ function checkProcessEnv(): boolean {
 
 function checkImportMeta(): boolean {
   /** @ts-ignore */
-  return typeof import.meta.env.VITE_LARAVEL_VUE_I18N_HAS_PHP !== 'undefined' &&
+  return typeof import.meta.env !== 'undefined' &&
     /** @ts-ignore */
     import.meta.env.VITE_LARAVEL_VUE_I18N_HAS_PHP
     ? true

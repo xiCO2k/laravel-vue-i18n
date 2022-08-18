330752ebb19f74c3c3b8735364035f16299580f8
ragulka <illimar.tambek@gmail.com>
2022-08-18 17:53:50 +0300

    Add PluginOptionsInterface

---
+++ src/interfaces/plugin-options.ts
@@ -0,0 +1,8 @@
+import { OptionsInterface } from './options'
+
+/**
+ * The Interface that is responsible for the Options provided.
+ */
+export interface PluginOptionsInterface extends OptionsInterface {
+  shared?: boolean
+}
